fd5dd0b5a5e3ca6f9411877ec542b9cace1af79c
Vishnu <r.vishnunandhan2024@vitstudent.ac.in>
2026-02-13 18:09:16 +0530

    structure error

--- langchain_agent.py
+++ langchain_agent.py
@@ -2,7 +2,7 @@ from typing import Dict, Any, Optional, Tuple
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from langchain_classic.memory import ConversationBufferMemory
-from langchain.tools import StructuredTool
+from langchain_core.tools import StructuredTool
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, SystemMessage
 from AgentClass import HFRouterCodeGenerator, run_python_chart, GenRequest, create_client, ChartResult
 
